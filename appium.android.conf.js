39d6ad7dda06d41d9a9429ad5508cf79a07e277c
sandeepqaops@gmail.com <sandeep@Sandeeps-MacBook-Pro.local>
2021-01-31 12:16:50 -0500

    increase timeouts

--- appium.android.conf.js
+++ appium.android.conf.js
@@ -4,9 +4,12 @@ const capabilities = {
     acceptInsecureCerts: false,
     appiumVersion: '1.20.0',
     platformName: 'Android',
-    platformVersion: '{EMULATOR-PLATFORM-VERSION}',
-    deviceName: '{EMULATOR-NAME}',
+    platformVersion: '10',
+    deviceName: 'emulator-5554',
     app: './android/app/build/outputs/apk/debug/app-debug.apk',
     automationName: 'UiAutomator2',
+    uiautomator2ServerLaunchTimeout: 300000,
+    uiautomator2ServerInstallTimeout: 300000,
+    uiautomator2ServerReadTimeout: 300000,
 };
 exports.capabilities = capabilities;
