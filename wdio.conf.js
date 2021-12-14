f20493d34c142daf6e088fa07db61e13f3653e07
sandeepqaops@gmail.com <sandeep@Sandeeps-MacBook-Pro.local>
2021-12-14 15:23:41 -0500

    checkin hub configs

--- wdio.conf.js
+++ wdio.conf.js
@@ -99,8 +99,10 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
-    port: 4723,
+    services: [
+        "lambdatest"
+      ],
+    buildName: 'kdinesh-lambda-test',
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
